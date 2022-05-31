348a7d04562215aa2b1f02d84c1e0bab0cbc99b5
JingxinJiang <jiajingx@sheridancollege.ca>
2022-05-30 22:12:02 -0400

    third

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const clientId=`?client_id=${process.env.REACT_APP_ACESS_KEY}`
+const clientId=`?client_id=0hmpEB61YL5n-pLQQBvEyfRbrg29pG6fEOXfBWuHu-0`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
